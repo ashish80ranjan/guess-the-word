5ccac831e49a3882b3fef3eb9b66b2b0ad7bed2e
Ashish Ranjan <ashish.ranjan.cse23@heritageit.edu.in>
2021-03-15 00:27:58 +0530

    initial commit

---
+++ js/script.js
@@ -0,0 +1,51 @@
+const msg=document.querySelector('.message');
+const guess=document.querySelector('input');
+const btn=document.querySelector('.btn');
+
+let arrword=['python','javascript','c++','php','java','reactjs','angular','swift','android','sql','html','css'];
+
+let neWords="";
+let scaWords="";
+
+
+const gessfunction=()=>{
+    let ran =Math.floor(Math.random()*arrword.length)
+    let temp =arrword[ran];
+    return temp;
+}
+
+const  scramble=(words)=>{
+    for(let i=words.length-1;i>0;i--)
+    {
+        let temp=words[i]
+        let j=Math.floor(Math.random()*(i+1));
+        words[i]=words[j]
+        words[j]=temp;
+    }
+    return words;
+};
+
+let play =false;
+btn.addEventListener('click',function(){
+    if(!play){ scramble(neWords);
+        play=true;
+        btn.innerHTML="Guess"
+        guess.classList.toggle('hidden');
+        neWords = gessfunction();
+        scaWords = scramble(neWords.split("")).join("");
+        msg.innerHTML=scaWords;
+    }  
+    else{
+        let enterword=guess.value;
+        if(enterword===neWords){
+            play=false;
+            msg.innerHTML=`Correct! it is ${neWords} `
+            btn.innerHTML="Start Again"
+            guess.classList.toggle('hidden');
+            guess.value="";
+
+        }else{
+            msg.innerHTML=`Worng! try Again`
+        }
+    }
+})
